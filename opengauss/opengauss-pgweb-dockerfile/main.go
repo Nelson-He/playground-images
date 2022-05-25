2243327af3b4c6f80e6ce81fb275fe61d8a8c0fc
zhaochunjiang <dakang_siji@189.cn>
2022-05-25 17:00:21 +0800

    update file

--- opengauss/opengauss-pgweb-dockerfile/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/sosedoff/pgweb/pkg/cli"
-)
-
-func main() {
-	cli.Run()
-}
--- opengauss/opengauss-pgweb-dockerfile/static/data.go
+++
@@ -1,7 +0,0 @@
-package static
-
-import "embed"
-
-//go:embed img/* js/* css/* fonts/*
-//go:embed index.html
-var Static embed.FS
