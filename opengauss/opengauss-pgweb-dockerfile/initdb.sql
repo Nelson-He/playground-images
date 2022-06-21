42fbc9a1b2e00e9c4cd8b60d4d6f99cee47e1f8e
Nelson-He <heguofeng@huawei.com>
2022-06-21 12:07:47 +0800

    授予openGauss用户权限，方便用户进行数据库操作

--- opengauss/opengauss-pgweb-dockerfile/initdb.sql
+++ opengauss/opengauss-pgweb-dockerfile/initdb.sql
@@ -1,3 +1,7 @@
 CREATE USER "openGauss" with createdb IDENTIFIED BY 'openGauss2022';
 
 CREATE DATABASE "openGauss" OWNER "openGauss" template = template0;
+
+GRANT ALL PRIVILEGES ON DATABASE "openGauss" to "openGauss";
+
+GRANT ALL PRIVILEGES  to "openGauss";
